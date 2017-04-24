c06298f686150f77b230717e48b35ab66a0e9d9a
Xi-Jun Lee <xijun.lee@hotmail.com>
2017-04-24 21:06:08 +0800

    modified

--- titanic_2.py
+++ titanic_2.py
@@ -65,44 +65,41 @@ for dataset in full_data:
 print (train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
 
 for dataset in full_data:
-        # Mapping Sex
-            dataset['Sex'] = dataset['Sex'].map( {'female': 0, 'male': 1}  ).astype(int)
-                
-                    # Mapping titles
-                        title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
-                            dataset['Title'] = dataset['Title'].map(title_mapping)
-                                dataset['Title'] = dataset['Title'].fillna(0)
-                                    
-                                        # Mapping Embarked
-                                            dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2}  ).astype(int)
-                                                
-                                                    # Mapping Fare
-                                                        dataset.loc[ dataset['Fare'] <= 7.91, 'Fare' ]                                = 0
-                                                            dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
-                                                                dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
-                                                                    dataset.loc[ dataset['Fare'] > 31, 'Fare' ]                                  = 3
-                                                                        dataset['Fare'] = dataset['Fare'].astype(int)
-                                                                            
-                                                                                # Mapping Age
-                                                                                    dataset.loc[ dataset['Age'] <= 16, 'Age' ]                         = 0
-                                                                                        dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
-                                                                                            dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
-                                                                                                dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
-                                                                                                    dataset.loc[ dataset['Age'] > 64, 'Age' ]                           = 4
+    # Mapping Sex
+    dataset['Sex'] = dataset['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+    
+    # Mapping titles
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+    
+    # Mapping Embarked
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+    
+    # Mapping Fare
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] 						        = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+    dataset.loc[ dataset['Fare'] > 31, 'Fare'] 							        = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+    
+    # Mapping Age
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] 					       = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age']                           = 4
 
-                                                                                                    # Feature Selection
-                                                                                                    drop_elements = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp',\
-                                                                                                                                     'Parch', 'FamilySize']
-                                                                                                    train = train.drop(drop_elements, axis = 1)
-                                                                                                    train = train.drop(['CategoricalAge', 'CategoricalFare'], axis = 1)
+# Feature Selection
+drop_elements = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp',\
+                 'Parch', 'FamilySize']
+train = train.drop(drop_elements, axis = 1)
+train = train.drop(['CategoricalAge', 'CategoricalFare'], axis = 1)
 
-                                                                                                    test  = test.drop(drop_elements, axis = 1)
+test  = test.drop(drop_elements, axis = 1)
 
-                                                                                                    print (train.head(10))
-
-                                                                                                    train = train.values
-                                                                                                    test  = test.values
-
+train = train.values
+test  = test.values
 
 import matplotlib.pyplot as plt
 import seaborn as sns
