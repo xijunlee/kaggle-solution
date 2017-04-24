ce624ff203a4fa1bcab51a1a7af5e8ccafb8a0d3
Xi-Jun Lee <xijun.lee@hotmail.com>
2017-04-24 21:00:31 +0800

    modified

--- titanic_2.py
+++ titanic_2.py
@@ -1,42 +1,127 @@
-# Load in our libraries
-import pandas as pd
+%matplotlib inline
 import numpy as np
-import re
-import sklearn
-#import xgboost as xgb
-import seaborn as sns
+import pandas as pd
+import re as re
+
+train = pd.read_csv('./train.csv', header = 0, dtype={'Age': np.float64})
+test  = pd.read_csv('./test.csv' , header = 0, dtype={'Age': np.float64})
+full_data = [train, test]
+
+print (train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean())
+
+print (train[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean())
+
+for dataset in full_data:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+print (train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean())
+
+for dataset in full_data:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+print (train[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
+
+for dataset in full_data:
+    dataset['Embarked'] = dataset['Embarked'].fillna('S')
+print (train[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean())
+
+for dataset in full_data:
+    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
+train['CategoricalFare'] = pd.qcut(train['Fare'], 4)
+print (train[['CategoricalFare', 'Survived']].groupby(['CategoricalFare'], as_index=False).mean())
+
+for dataset in full_data:
+    age_avg 	   = dataset['Age'].mean()
+    age_std 	   = dataset['Age'].std()
+    age_null_count = dataset['Age'].isnull().sum()
+    
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
+    dataset['Age'] = dataset['Age'].astype(int)
+    
+train['CategoricalAge'] = pd.cut(train['Age'], 5)
+
+print (train[['CategoricalAge', 'Survived']].groupby(['CategoricalAge'], as_index=False).mean())
+
+def get_title(name):
+	title_search = re.search(' ([A-Za-z]+)\.', name)
+	# If the title exists, extract and return it.
+	if title_search:
+		return title_search.group(1)
+	return ""
+
+for dataset in full_data:
+    dataset['Title'] = dataset['Name'].apply(get_title)
+
+print(pd.crosstab(train['Title'], train['Sex']))
+
+for dataset in full_data:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
+ 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+print (train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+
 import matplotlib.pyplot as plt
-# matplotlib inline
+import seaborn as sns
 
-import plotly.offline as py
-#py.init_notebook_mode(connected=True)
-import plotly.graph_objs as go
-import plotly.tools as tls
-
-# Going to use these 5 base models for the stacking
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.cross_validation import KFold
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
 
-# Load in the train and test datasets
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(probability=True),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(),
+	AdaBoostClassifier(),
+    GradientBoostingClassifier(),
+    GaussianNB(),
+    LinearDiscriminantAnalysis(),
+    QuadraticDiscriminantAnalysis(),
+    LogisticRegression()]
 
-# Store our passenger ID for easy access
-PassengerId = test['PassengerId']
+log_cols = ["Classifier", "Accuracy"]
+log 	 = pd.DataFrame(columns=log_cols)
 
-#print (train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean())
+sss = StratifiedShuffleSplit(n_splits=10, test_size=0.1, random_state=0)
 
-#print (train[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean())
+X = train[0::, 1::]
+y = train[0::, 0]
 
-train = train.values
-test = test.values
+acc_dict = {}
 
-print type(train)
+for train_index, test_index in sss.split(X, y):
+	X_train, X_test = X[train_index], X[test_index]
+	y_train, y_test = y[train_index], y[test_index]
+	
+	for clf in classifiers:
+		name = clf.__class__.__name__
+		clf.fit(X_train, y_train)
+		train_predictions = clf.predict(X_test)
+		acc = accuracy_score(y_test, train_predictions)
+		if name in acc_dict:
+			acc_dict[name] += acc
+		else:
+			acc_dict[name] = acc
 
-X = train[:,1:]
-Y = train[:,0]
+for clf in acc_dict:
+	acc_dict[clf] = acc_dict[clf] / 10.0
+	log_entry = pd.DataFrame([[clf, acc_dict[clf]]], columns=log_cols)
+	log = log.append(log_entry)
 
-print X
+plt.xlabel('Accuracy')
+plt.title('Classifier Accuracy')
 
-print Y
+sns.set_color_codes("muted")
+sns.barplot(x='Accuracy', y='Classifier', data=log, color="b")
+
+
