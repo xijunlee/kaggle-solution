f2b2731824803ea0e163ed30388296dfaa132f3a
Xi-Jun Lee <xijun.lee@hotmail.com>
2017-04-27 16:29:40 +0800

    modified

--- House Prices/house_price.py
+++ House Prices/house_price.py
@@ -14,7 +14,59 @@ import warnings
 warnings.filterwarnings('ignore')
 #%matplotlib inline
 
-#bring in the six packs
 df_train = pd.read_csv('./train.csv')
 
-print df_train['SalePrice'].describe()
+#print df_train['SalePrice'].index
+
+#sns.distplot(df_train['SalePrice']);
+
+#scatter plot grlivarea/saleprice
+
+'''
+var = 'GrLivArea'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+'''
+'''
+#scatter plot totalbsmtsf/saleprice
+var = 'TotalBsmtSF'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+'''
+
+'''
+#box plot overallqual/saleprice
+var = 'OverallQual'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+'''
+
+'''
+var = 'YearBuilt'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(16, 8))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+plt.xticks(rotation=75);
+'''
+
+'''
+# 窝草 太强了，居然可以直接对caterical variable作相关性分析，不用将其数值化。直接调用函数也太方便了。
+#correlation matrix
+corrmat = df_train.corr()
+#saleprice correlation matrix
+k = 10 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+'''
+
+#scatterplot
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(df_train[cols], size = 1.0)
+
+plt.show()
