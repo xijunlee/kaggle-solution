0f72f509d33a31095e8e2ae0ca74f1eb705ae366
xijun.lee <xijun.lee@hotmail.com>
2017-06-23 10:28:47 +0800

    modified

--- DigitRecognizer/DigitRec_CNN.py
+++ DigitRecognizer/DigitRec_CNN.py
@@ -4,14 +4,16 @@ import keras.layers.core as core
 import keras.layers.convolutional as conv
 import keras.models as models
 import keras.utils.np_utils as kutils
+import pdb
 
 # The competition datafiles are in the directory ../input
 # Read competition data files:
-train_df = pd.read_csv("../train.csv").values
-test_df  = pd.read_csv("../test.csv").values
+train_df = pd.read_csv("../../train.csv")
+test_df  = pd.read_csv("../../test.csv")
 
 train = train_df.values
 test = test_df.values
+
 
 nb_epoch = 1 # Change to 100
 
@@ -32,12 +34,12 @@ nb_classes = trainY.shape[1]
 
 cnn = models.Sequential()
 
-cnn.add(conv.Convolution2D(nb_filters_1, nb_conv, nb_conv,  activation="relu", input_shape=(28, 28, 1), border_mode='same'))
-cnn.add(conv.Convolution2D(nb_filters_1, nb_conv, nb_conv, activation="relu", border_mode='same'))
+cnn.add(conv.Conv2D(nb_filters_1, nb_conv, nb_conv,  activation="relu", input_shape=(28, 28, 1), border_mode='same'))
+cnn.add(conv.Conv2D(nb_filters_1, nb_conv, nb_conv, activation="relu", border_mode='same'))
 cnn.add(conv.MaxPooling2D(strides=(2,2)))
 
-cnn.add(conv.Convolution2D(nb_filters_2, nb_conv, nb_conv, activation="relu", border_mode='same'))
-cnn.add(conv.Convolution2D(nb_filters_2, nb_conv, nb_conv, activation="relu", border_mode='same'))
+cnn.add(conv.Conv2D(nb_filters_2, nb_conv, nb_conv, activation="relu", border_mode='same'))
+cnn.add(conv.Conv2D(nb_filters_2, nb_conv, nb_conv, activation="relu", border_mode='same'))
 cnn.add(conv.MaxPooling2D(strides=(2,2)))
 
 #cnn.add(conv.Convolution2D(nb_filters_3, nb_conv, nb_conv, activation="relu", border_mode='same'))
@@ -62,6 +64,7 @@ testX /= 255.0
 
 yPred = cnn.predict_classes(testX)
 
-Submission = pd.DataFrame({'ImageId':test['ImageId'],'Label':yPred})
+imageId = [i for i in range(1,len(yPred)+1)]
+Submission = pd.DataFrame({'ImageId':imageId,'Label':yPred})
 Submission.to_csv('Submission.csv', index = False)
-print 'Results saved to .csv successfully!'
+print ('Results saved to .csv successfully!')
