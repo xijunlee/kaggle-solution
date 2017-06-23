04b55ff71f581aa4989d5f3ebba887b0eb35524e
xijun.lee <xijun.lee@hotmail.com>
2017-06-23 15:00:56 +0800

    modified

--- DigitRecognizer/DigitRec_SVM.py
+++ DigitRecognizer/DigitRec_SVM.py
@@ -20,16 +20,30 @@ def standardize(x):
     std = x.std().astype(np.float32)
     return (x-mean)/std
 
+print ('Standardizing the original data ...')
 # Standardizing the original data
 train_x = standardize(train_x)
 test_x = standardize(test_x)
 
+print ('PCA ...')
 nb_components = 15*15
 # Principle component analysis
 pca = decomposition.PCA(n_components = nb_components)
 pca.fit(train_x)
 train_x = pca.transform(train_x)
+print (train_x.shape)
 
-clf = svm.SVC(kernel='rbf', C=1)
-scores = cross_val_score(clf,train_x,train_y,cv=5)
-print scores
+print ('Model fitting ...')
+clf = SVC(kernel='rbf', C=1)
+#scores = cross_val_score(clf,train_x,train_y,cv=5)
+clf.fit(train_x,train_y)
+print ('PCA fit on test_x ... ')
+pca.fit(test_x)
+test_x = pca.transform(test_x)
+print ('Predicting ...')
+yPreds = clf.predict(test_x)
+imageId = [i for i in range(1,len(yPreds)+1)]
+submission = pd.DataFrame({'ImageId':imageId,'Label':yPreds})
+submission.to_csv('submission_svm.csv',index = False)
+print ('Result saved to .csv successfully!')
+
