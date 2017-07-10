4647201a777a38035ae1bca81bc3a29f66f6b232
XIJUN@MARK-II <xijun.lee@hotmail.com>
2017-07-10 16:20:53 +0800

    modified

--- calcIntegrate.py
+++ calcIntegrate.py
@@ -2,9 +2,33 @@
 # coding=utf-8
 
 from sympy import *
+import numpy as np
+import pandas as pd
 
-x, r = symbols('x r')
-expr1 = 1-1/sqrt(2*pi)*integrate(exp(-x**2/2),(x,-oo,r))
-expr2 = 1/sqrt(2*pi)*exp(-r**2/2)
-result = solve(r-expr1/expr2,r)
-print result
+x, r = symbols('x')
+vl, vh = 10.0, 100.0
+lambs = [1,2,3,4]
+T = 3
+f = 1.0/(vh-vl)
+
+Rs = []
+for lamb in lambs:
+	R = []
+	for r in xrange(int(vl),int(vh),1):
+		integral = integrate((x-vl)/(vh-vl)+x/(vh-vl)*exp(-lamb*T*(1-(r-vl)/(vh-vl))*(1-(x-vl)/(vh-vl))),(x,r,vh))
+		R.append(lamb*T*integral)
+	Rs.append(R)
+Rs = np.array(Rs)
+Rs = np.transpose(Rs)
+
+df = pd.DataFrame({
+	'lambda=1':Rs[:,0],
+	'lambda=2':Rs[:,1],
+	'lambda=3':Rs[:,2],
+	'lambda=4':Rs[:,3]
+	})
+
+print df.head()
+
+df.to_csv('T=3.csv',index=False )
+
