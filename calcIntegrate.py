f3b7c650f1c50a543c2878749af6649b3dd6ee00
xijun.lee <xijun.lee@hotmail.com>
2017-07-11 09:52:36 +0800

    modified

--- calcIntegrate.py
+++ calcIntegrate.py
@@ -6,26 +6,27 @@ import numpy as np
 import pandas as pd
 
 x = symbols('x')
-vl, vh = 100,200
-lambs = [1,2,3,4]
-T = 7
+vl, vh = 100.0,200.0
+lambs = [1.0,2.0,3.0,4.0]
+T = 7.0
 f = 1.0/(vh-vl)
 
 Rs = []
 for lamb in lambs:
 	R = []
 	for r in xrange(int(vl),int(vh),2):
-		integral = integrate((x-vl)/(vh-vl)+x/(vh-vl)*exp(-lamb*T*(1.0-(r-vl)/(vh-vl))*(1.0-(x-vl)/(vh-vl))),(x,r,vh))
-		R.append(lamb*T*integral)
+		#integral = integrate((x-vl)/(vh-vl)+x/(vh-vl)*exp(-lamb*T*(1.0-(r-vl)/(vh-vl))*(1.0-(x-vl)/(vh-vl))),(x,r,vh))
+		integral = integrate(((x-vl)/(vh-vl)+x/(vh-vl))*exp(-lamb*T*(1-(r-vl)/(vh-vl))*(1-(x-vl)/(vh-vl))),(x,r,vh))
+                R.append(lamb*T*integral)
 	Rs.append(R)
 Rs = np.array(Rs)
 Rs = np.transpose(Rs)
 
 df = pd.DataFrame({
-	'lambda=1':Rs[:,0],
-	'lambda=2':Rs[:,1],
-	'lambda=3':Rs[:,2],
-	'lambda=4':Rs[:,3]
+	'1':Rs[:,0],
+	'2':Rs[:,1],
+	'3':Rs[:,2],
+	'4':Rs[:,3]
 	})
 
 print df.head()
